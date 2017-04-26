c4d2fdf5bc8fc697e3c25ebab403ebfd3d01cf5f
burchard <ryszard.burchardt@nokia.com>
2017-04-26 15:21:29 +0200

    Import whole ReactDOM in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import Layout from './app/Layout';
 
-render(<Layout />, document.getElementById('root'));
+const app = document.getElementById('root')
+ReactDOM.render(<Layout />, app);
